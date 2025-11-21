ec70526f50ee69f0926835c3db45c5d51b93daff
Michael McBride <mcbride1987@gmail.com>
2025-11-21 13:01:59 -0800

    update package.json for cors and swagger.  Update server.js for api-doc endpoint with swagger. Implement swagger.yaml for full documention of all endpoints.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import usersRouter from "./routes/users.js";
+import { swaggerUi, swaggerDocument } from "./swagger.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", usersRouter);
 
